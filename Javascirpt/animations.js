291d05e22993444d6c3c2bc03f557e6e8cb4abf4
Alex333S <150231712+Alex333S@users.noreply.github.com>
2024-06-28 14:20:55 +1200

    website for webdev

---
+++ Javascirpt/animations.js
@@ -0,0 +1,19 @@
+anime({
+    targets: '.dir-normal',
+    translateX: 250,
+    easing: 'easeInOutSine'
+  });
+  
+  anime({
+    targets: '.dir-reverse',
+    translateX: 250,
+    direction: 'reverse',
+    easing: 'easeInOutSine'
+  });
+  
+  anime({
+    targets: '.dir-alternate',
+    translateX: 250,
+    direction: 'alternate',
+    easing: 'easeInOutSine'
+  });
